7c179192d20f3313665fbfe4693d9a08f83d7024
machachuchez <61087602+pssydestroyer@users.noreply.github.com>
2024-02-26 14:03:49 +0200

    New settings
    ∙More options have been added to the config file. Now, the GPT settings are changed there, not in the bot itself.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+#-------API tokens---------#
 TG_token = ''
 TELEGRAPH_token = ''
 GPT_api = ''
+
+#-------GPT settings-------#
+GPT_MODEL = 'gpt-3.5-turbo-16k'
+Temperature = 0.1 # кароч, чим більий параметр тим більше води буде лити гпт
+MAX_TOKEN = 16043 # чим більше токенів тим крутіше (стаття буде більша)
